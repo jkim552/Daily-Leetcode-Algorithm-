8252e48846c30cfa50e013133b9c5d42e72d21b2
jkim552 <claymorejh4@gmail.com>
2022-08-03 01:10:03 -0400

    Day 1

---
+++ BinarySearch/BinarySearch.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  var index = -1;
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] === target) {
+      index = i;
+    }
+  }
+  return index;
+};
